2339285fead41b7701ad4333e23b4a74efbb5425
InstincT <113701884+monish-instinct@users.noreply.github.com>
2024-11-19 12:25:02 +0530

    Add files via upload

---
+++ Binary.xcframework/macos-arm64/Binary.framework/Versions/A/Headers/Binary.h
@@ -0,0 +1,18 @@
+//
+//  Binary.h
+//  Binary
+//
+//  Created by ravichandran raju on 19/11/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Binary.
+FOUNDATION_EXPORT double BinaryVersionNumber;
+
+//! Project version string for Binary.
+FOUNDATION_EXPORT const unsigned char BinaryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Binary/PublicHeader.h>
+
+
